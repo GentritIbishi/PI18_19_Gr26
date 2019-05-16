2c548f7d8628ab73b786d1745255ea96184c10b3
EldinVeliji <44035030+EldinVeliji@users.noreply.github.com>
2019-05-17 01:51:28 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,129 +1,157 @@
-<footer>
-  <div class="footer">
-  <div class="contain">
-  <div class="col">
-    <h1>Company</h1>
-    <ul>
-      <li>About</li>
-      <li>Mission</li>
-      <li>Services</li>
-      <li>Social</li>
-      <li>Get in touch</li>
-    </ul>
-  </div>
-  <div class="col">
-    <h1>Products</h1>
-    <ul>
-      <li>About</li>
-      <li>Mission</li>
-      <li>Services</li>
-      <li>Social</li>
-      <li>Get in touch</li>
-    </ul>
-  </div>
-  <div class="col">
-    <h1>Accounts</h1>
-    <ul>
-      <li>About</li>
-      <li>Mission</li>
-      <li>Services</li>
-      <li>Social</li>
-      <li>Get in touch</li>
-    </ul>
-  </div>
-  <div class="col">
-    <h1>Resources</h1>
-    <ul>
-      <li>Webmail</li>
-      <li>Redeem code</li>
-      <li>WHOIS lookup</li>
-      <li>Site map</li>
-      <li>Web templates</li>
-      <li>Email templates</li>
-    </ul>
-  </div>
-  <div class="col">
-    <h1>Support</h1>
-    <ul>
-      <li>Contact us</li>
-      <li>Web chat</li>
-      <li>Open ticket</li>
-    </ul>
-  </div>
-  <div class="col social">
-    <h1>Social</h1>
-    <ul>
-      <li><img src="https://svgshare.com/i/5fq.svg" width="32" style="width: 32px;"></li>
-      <li><img src="https://svgshare.com/i/5eA.svg" width="32" style="width: 32px;"></li>
-      <li><img src="https://svgshare.com/i/5f_.svg" width="32" style="width: 32px;"></li>
-    </ul>
-  </div>
-<div class="clearfix"></div>
-</div>
-</div>
-<!-- END OF FOOTER -->
-
-</footer>
-<style>
-/* STYLES SPECIFIC TO FOOTER  */
-.footer {
-width: 100%;
-position: relative;
-height: auto;
-background-color:#e5e5e5;
-}
-.footer .col {
-width: 190px;
-height: auto;
-float: left;
-box-sizing: border-box;
--webkit-box-sizing: border-box;
--moz-box-sizing: border-box;
-padding: 0px 20px 20px 20px;
-}
-.footer .col h1 {
-margin: 0;
-padding: 0;
-font-family: inherit;
-font-size: 12px;
-line-height: 17px;
-padding: 20px 0px 5px 0px;
-
-font-weight: normal;
-text-transform: uppercase;
-letter-spacing: 0.250em;
-}
-.footer .col ul {
-list-style-type: none;
-margin: 0;
-padding: 0;
-}
-.footer .col ul li {
-color: #999999;
-font-size: 14px;
-font-family: inherit;
-font-weight: bold;
-padding: 5px 0px 5px 0px;
-cursor: pointer;
-transition: .2s;
--webkit-transition: .2s;
--moz-transition: .2s;
-}
-.social ul li {
-display: inline-block;
-padding-right: 5px !important;
-}
-
-.footer .col ul li:hover {
-color: #838383;
-transition: .1s;
--webkit-transition: .1s;
--moz-transition: .1s;
-}
-.clearfix {
-clear: both;
-}
-
-</style>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="footer.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+</head>
+<body>    
+        <div class="footer-dark">
+                <footer>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-3 item">
+                                <h3>Time</h3>
+                                <ul>
+                                    <li><p> | New York: <span class="clock" data-timezone="America/New_York"></span></p></li>
+                                    <li><p> | Shanghai: <span class="clock" data-timezone="Asia/Shanghai"></span></p></li>
+                                    <li><p> | Berlin: <span class="clock" data-timezone="Europe/Berlin"></span></p></li>
+                                    <li><p> | Moscow <span class="clock" data-timezone="Europe/Moscow"></span></p></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-6 col-md-3 item">
+                                <h3>About</h3>
+                                <ul>
+                                    <li id="footer-li"><a href="#">Company</a></li>
+                                    <li id="footer-li"><a href="#">Team</a></li>
+                                    <li id="footer-li"><a href="#">Careers</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6 item text">
+                                <h3>Company Name</h3>
+                                <p class="footer-p">Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                            </div>
+                            <div class="col item social"><a href="#"><i class="fab fa-facebook"></i></a><a href="#"><i class="fab fa-twitter"></i></a><a href="#"><i class="fab fa-snapchat-ghost"></i></a><a href="#"><i class="fab fa-instagram"></i></a></div>
+                        </div>
+                        <p class="copyright">Company Name © 2018</p>
+                    </div>
+                </footer>
+              </div>
+              <script>
+                        const clocks = document.getElementsByClassName("clock");
+                        
+                        function updateClocks() {
+                            for (let clock of clocks) {
+                            let timezone = clock.dataset.timezone;
+                            let time = new Date().toLocaleTimeString("en-US", {
+                                hour: '2-digit',
+                                minute:'2-digit',
+                                timeZone: timezone
+                            });
+                            clock.textContent = time;
+                            }
+                        }
+                        
+                        // Update every minute:
+                        setInterval(updateClocks, 60000);
+                        updateClocks();
+                </script>
+                            
 </body>
 </html>
+<style>
+.footer-dark {
+    padding:50px 0;
+    color:#f0f9ff;
+    background-color:#282d32;
+  }
+  
+  .footer-dark h3 {
+    margin-top:0;
+    margin-bottom:12px;
+    font-weight:bold;
+    font-size:16px;
+  }
+  
+  .footer-dark ul {
+    padding:0;
+    list-style:none;
+    line-height:1.6;
+    font-size:14px;
+    margin-bottom:0;
+  }
+  
+  .footer-dark ul a {
+    color:inherit;
+    text-decoration:none;
+    opacity:0.6;
+  }
+  
+  .footer-dark ul a:hover {
+    opacity:0.8;
+  }
+  .footer-p {
+    font-size:14px;
+  }
+  
+  @media (max-width:767px) {
+    .footer-dark .item:not(.social) {
+      text-align:center;
+      padding-bottom:20px;
+    }
+  }
+  
+  .footer-dark .item.text {
+    margin-bottom:36px;
+  }
+  
+  @media (max-width:767px) {
+    .footer-dark .item.text {
+      margin-bottom:0;
+    }
+  }
+  
+  .footer-dark .item.text .footer-p {
+    opacity:0.6;
+    margin-bottom:0;
+  }
+  
+  .footer-dark .item.social {
+    text-align:center;
+  }
+  
+  @media (max-width:991px) {
+    .footer-dark .item.social {
+      text-align:center;
+      margin-top:20px;
+    }
+  }
+  
+  .footer-dark .item.social > a {
+    font-size:20px;
+    width:36px;
+    height:36px;
+    line-height:36px;
+    display:inline-block;
+    text-align:center;
+    border-radius:50%;
+    box-shadow:0 0 0 1px rgba(255,255,255,0.4);
+    margin:0 8px;
+    color:#fff;
+    opacity:0.75;
+  }
+  
+  .footer-dark .item.social > a:hover {
+    opacity:0.9;
+  }
+  
+  .footer-dark .copyright {
+    text-align:center;
+    padding-top:24px;
+    opacity:0.3;
+    font-size:13px;
+    margin-bottom:0;
+  }
+  .clock {
+    font-family: sans-serif;
+ }
+ </style>
