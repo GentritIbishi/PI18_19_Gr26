baec44d21d86aeae1c49899f6f0bd31a06cda0ed
EldinVeliji <44035030+EldinVeliji@users.noreply.github.com>
2019-05-17 01:56:20 +0200

    Add files via upload

---
+++ oneplus6t.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html>
+<?php
+require "header.php";
+?>
+<head>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  <title>Oneplus 6t</title>
+</head>
+<link rel="stylesheet" href="CSS/sgsfold.css">
+<body>
+
+<main>
+        <table>
+            <thead>
+                <tr>
+                    <th><img src="Pictures/oneplus6t.jpg" alt=""></th>
+                    <th><i class="fas fa-microchip"></i> 256GB Storage | <i class="fas fa-mobile-alt"></i> 6.41 inches | <i class="fas fa-battery-full"></i> 3700mAh |</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td data-label="Name">
+                        LAUNCHED
+                    </td>
+                    <td data-label="Grid">
+                       <table class="basic-table">
+                        <tr id="basic-row"><td id="basic-data">| Anouncement|</td></tr>
+                        <tr id="basic-row"><td id="basic-data">| Status      |</td></tr>
+                       </table>
+                    </td>
+                    <td data-label="Type">
+                        <table class="basic-table">
+                            <tr id="basic-row"><td id="basic-data">2018, October</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Avaible, Released 2018, November</td></tr>
+                           </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        NETWORK
+                    </td>
+                    <td data-label="Grid">
+                        | Technology |
+                    </td>
+                    <td data-label="Type">
+                        GSM/HSPA/LTE/5G
+                    </td>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        BODY
+                    </td>
+                    <td data-label="Grid">
+                        <table class="basic-table">
+                            <tr id="basic-row"><td id="basic-data">| Dimensions |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Weight |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| SIM |</td></tr> 
+                          </table>
+                    </td>
+                    <td data-label="Type">
+                        <table class="basic-table">
+                            <tr id="basic-row"><td id="basic-data">157.5 x 74.8 x 8.2 mm (6.20 x 2.94 x 0.32 in)</td></tr>
+                            <tr id="basic-row"><td id="basic-data">185 g (6.53 oz)</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Dual SIM (Nano-SIM, dual stand-by)</td></tr>
+                          </table>
+                    </td>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        DISPLAY
+                    </td>
+                    <td data-label="Grid">
+                        <table>
+                          <tr id="basic-row"><td id="basic-data">| Type |</td></tr>
+                          <tr id="basic-row"><td id="basic-data">| Size |</td></tr>
+                          <tr id="basic-row"><td id="basic-data">| Resolution |</td></tr>
+                          <tr id="basic-row"><td id="basic-data">| Protection |</td></tr>
+                        </table> 
+                    </td>
+                    <td data-label="Type">
+                      <table>
+                          <tr id="basic-row"><td id="basic-data">Optic AMOLED capacitive touchscreen, 16M colors</td></tr>
+                          <tr id="basic-row"><td id="basic-data">6.41 inches, 100.9 cm2 (~85.6% screen-to-body ratio)</td></tr>
+                          <tr id="basic-row"><td id="basic-data">1080 x 2340 pixels, 19.5:9 ratio (~402 ppi density)</td></tr>
+                          <tr id="basic-row"><td id="basic-data">Corning Gorilla Glass 6</td></tr>
+                      </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        PLATFORM
+                    </td>
+                    <td data-label="Grid">
+                      <table>
+                        <tr id="basic-row"><td id="basic-data">| OS |</td></tr>
+                        <tr id="basic-row"><td id="basic-data">| Chipset |</td></tr>
+                        <tr id="basic-row"><td id="basic-data">| CPU |</td></tr>
+                        <tr id="basic-row"><td id="basic-data">| GPU |</td></tr>
+                      </table>
+                    </td>
+                    <td data-label="Type">
+                      <table>
+                        <tr id="basic-row"><td id="basic-data">Android 9.0 (Pie); OxygemOS 9.0.5</td></tr>
+                        <tr id="basic-row"><td id="basic-data">Qualcomm SDM855 Snapdragon 845 (10 nm)</td></tr>
+                        <tr id="basic-row"><td id="basic-data">Octa-core (4x2.8 GHz Kryo 385 Gold & 4x1.7 GHz Kryo 385 Silver)</td></tr>
+                        <tr id="basic-row"><td id="basic-data">Adreno 630</td></tr>
+                      </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        MEMORY
+                    </td>
+                    <td data-label="Grid">
+                      <table>
+                        <tr id="basic-row"><td id="basic-data">| Card slot |</td></tr>
+                        <tr id="basic-row"><td id="basic-data">| Internal  |</td></tr>
+                      </table>
+                    </td>
+                    <td data-label="Type">
+                      <table>
+                        <tr id="basic-row"><td id="basic-data">No</td></tr>
+                        <tr id="basic-row"><td id="basic-data">256 GB, 8 GB RAM, 128GB, 6/8 GB RAM</td></tr>
+                      </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        MAIN CAMERA
+                    </td>
+                    <td data-label="Grid">
+                      <table>
+                        <tr id="basic-row"><td id="basic-data">| Double |</td></tr>
+                        <tr id="basic-row"><td id="basic-data">.</td></tr>
+                        <tr id="basic-row"><td id="basic-data">| Features |</td></tr>
+                        <tr id="basic-row"><td id="basic-data">| Video |</td></tr>
+                      </table>
+                    </td>
+                    <td data-label="Type">
+                      <table>
+                        <tr id="basic-row"><td id="basic-data">16 MP, f/1.7, 25mm (wide), 1/2.6", 1.22µm, OIS, PDAF</td></tr>
+                        <tr id="basic-row"><td id="basic-data">20 MP (16 MP effective), f/1.7, 25mm (wide), 1/2.8", 1.0µm, PDAF</td></tr>
+                        <tr id="basic-row"><td id="basic-data">Dual-LED flash, HDR, panorama</td></tr>
+                        <tr id="basic-row"><td id="basic-data">2160p@60fps, 1080p@60/240fps (gyro-EIS), 720p@480fps, Auto HDR, gyro-EIS</td></tr>
+                      </table>
+                    </td>
+                    <!--<td data-label="Chickens">
+                        <span class="no">&#10007;</span>
+                    </td>
+                    <td data-label="Pigs">
+                        <span class="yes">&#10003;</span>
+                    </td>
+                    <td data-label="Snakes">
+                        <span class="no">&#10007;</span>
+                    -->
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        SELFIE CAMERA
+                    </td>
+                    <td data-label="Grid">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">| Single |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Features |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Video |</td></tr>
+                          </table>   
+                    </td>
+                    <td data-label="Type">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">16 MP, f/2.0, 25mm (wide), 1/3.1", 1.0µm</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Auto-HDR, gyro-EIS</td></tr>
+                            <tr id="basic-row"><td id="basic-data">1080p@30fps</td></tr>
+                          </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        SOUND
+                    </td>
+                    <td data-label="Grid">
+                      <table>
+                        <tr id="basic-row"><td id="basic-data">| Loudspeaker |</td></tr>                       
+                        <tr id="basic-row"><td id="basic-data">| 3.5mm jack |</td></tr>
+                        <tr id="basic-row"><td id="basic-data">.</td></tr>
+                        <tr id="basic-row"><td id="basic-data">.</td></tr>
+                      </table>
+                    </td>
+                    <td data-label="Type">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">Yes</td></tr>
+                            <tr id="basic-row"><td id="basic-data">No</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Active noise cancellation with dedicated mic</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Dirac HD Sound</td></tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        COMMS
+                    </td>
+                    <td data-label="Grid">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">| WLAN |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Bluetooth |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| GPS |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| NFC |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Radio |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| USB |</td></tr>
+                          </table>   
+                    </td>
+                    <td data-label="Type">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">Wi-Fi 802.11 a/b/g/n/ac, dual-band, WiFi Direct, DLNA, hotspot</td></tr>
+                            <tr id="basic-row"><td id="basic-data">5.0, A2DP, aptX HD, LE</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Yes, with A-GPS, GLONASS, BDS, GALILEO</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Yes</td></tr>
+                            <tr id="basic-row"><td id="basic-data">No</td></tr>
+                            <tr id="basic-row"><td id="basic-data">2.0, Type-C 1.0 reversible connector, USB On-The-Go</td></tr>
+                          </table>   
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        FEATURES
+                    </td>
+                    <td data-label="Grid">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">| Sensors |</td></tr>
+                        </table>
+                    </td>
+                    <td data-label="Type">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">Fingerprint (under display), accelerometer, gyro, proximity, compass</td></tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        BATTERY
+                    </td>
+                    <td data-label="Grid">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">| Battery |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Charging |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">.</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Stand by |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Talk time |</td></tr>
+                        </table>
+                    </td>
+                    <td data-label="Type">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">Non-removable Li-Po 3700 mAh battery</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Fast battery charging 20W</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Fast wireless charging 15W</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Not tested yet</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Not tested yet</td></tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        MISCELANEOUS
+                    </td>
+                    <td data-label="Grid">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">| Colors |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Models |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Price |</td></tr>
+                        </table>
+                    </td>
+                    <td data-label="Type">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">Thunder Purple, Mirror Black, Midnight Black</td></tr>
+                            <tr id="basic-row"><td id="basic-data">A6010, A6013</td></tr>
+                            <tr id="basic-row"><td id="basic-data">About 550 EUR</td></tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td data-label="Name">
+                        TESTS
+                    </td>
+                    <td data-label="Grid">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">| Performance |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Display |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Camera |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Loudspeaker |</td></tr>
+                            <tr id="basic-row"><td id="basic-data">| Battery life |</td></tr>  
+                        </table>
+                    </td>
+                    <td data-label="Type">
+                        <table>
+                            <tr id="basic-row"><td id="basic-data">Basemark OS II: 4941 / Basemark OS II 2.0: 4452</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Contrast ratio: Infinite (nominal), 4.138 (sunlight)</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Photo / Video</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Voice 67dB / Noise 72dB / Ring 84dB</td></tr>
+                            <tr id="basic-row"><td id="basic-data">Endurance rating 90h</td></tr>  
+                        </table>
+                    </td>
+                </tr>
+                </main> 
+
+
+
+</body>
+
+</html>
