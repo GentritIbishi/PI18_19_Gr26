9849183b82ebc6448349a25b830d01448f81d8f0
EldinVeliji <44035030+EldinVeliji@users.noreply.github.com>
2019-05-27 01:15:10 +0200

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,162 @@
+<html>
+<head>
+    <title>About Us</title>
+</head>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="CSS/about-us.css">
+<link rel="stylesheet" href="JS/about-us.js">
+<body>
+    <!--*************************About Us***********************************-->
+
+    <header id="main-header">
+            <div class="menu-toggle" id="hamburger">
+                <i class="fas fa-bars"></i>
+            </div>
+            <div class="overlay"></div>
+            <div class="head-container">
+                <nav class="head-nav">
+                    <h1 class="brand"><a href="index.php">GEE<span>K</span>PHONE</a></h1>
+                    <ul class="head-ul">
+                        <li id="header-li"><a id="header-a" href="index.php">Home</a></li>
+                        <li id="header-li"><a id="header-a" href="#">Services</a></li>
+                        <li id="header-li"><a id="header-a" href="about-us.php">About</a></li>
+                        <li id="header-li"><a id="header-a" href="contact-us.php">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+    </header>
+
+
+ <nav></nav>  
+ <div class="header">
+ 
+ </div>
+ 
+ <div class="container">
+    
+    <div class="about">
+       <div class="left">
+          <h1>About us</h1>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+ 
+                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+ 
+       </div>
+       <div class="right">
+          <img src="Pictures/about-us-photo1.jpg"height="700px">
+       </div>
+       <div class="clear"></div>
+    </div>
+    
+    
+    
+    <div class="mission">
+      
+       <div class="left">
+          <img src="Pictures/about-us-photo2.jpg"height="650px">
+       </div>
+         <div class="right">
+          <h1>Mission Statement</h1>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+ 
+                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+ 
+       </div>
+       <div class="clear"></div>
+       
+    </div>
+    
+    
+   
+      
+    
+    
+    
+    
+    <!--*****************Rreth nesh*****************-->
+    <figure class="snip0053 yellow">
+        <figcaption>
+          <h2>Eldin <span>Veliji</span></h2>
+          <p>I propose we leave math to the machines and go play outside.</p>
+          <div class="icons">
+            <a href="#"><i class="ion-ios-home"></i></a>
+            <a href="#"><i class="ion-ios-email"></i></a>
+            <a href="#"><i class="ion-ios-telephone"></i></a>
+          </div>
+        </figcaption>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample5.jpg" alt="sample5" />
+        <div class="position">Managing Director</div>
+      </figure>
+      <figure class="snip0053 blue">
+        <figcaption>
+          <h2>Gentrit <span>Ibishi</span></h2>
+          <p>Who wouldn't be interested in absolutely everything we try to do?!</p>
+          <div class="icons">
+            <a href="#"><i class="ion-ios-home"></i></a>
+            <a href="#"><i class="ion-ios-email"></i></a>
+            <a href="#"><i class="ion-ios-telephone"></i></a>
+          </div>
+        </figcaption>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample6.jpg" alt="sample6" />
+        <div class="position">Graphic Designer</div>
+      </figure>
+    
+        
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <div class="footer-dark">
+        <footer>
+            <div class="footer-container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Time</h3>
+                        <ul id="footer-ul">
+                            <li><p>New York: <span class="clock" data-timezone="America/New_York"></span></p></li>
+                            <li><p>Shanghai: <span class="clock" data-timezone="Asia/Shanghai"></span></p></li>
+                            <li><p>Berlin: <span class="clock" data-timezone="Europe/Berlin"></span></p></li>
+                            <li><p>Moscow <span class="clock" data-timezone="Europe/Moscow"></span></p></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul id="footer-ul">
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Company Name</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="fab fa-facebook"></i></a><a href="#"><i class="fab fa-twitter"></i></a><a href="#"><i class="fab fa-snapchat-ghost"></i></a><a href="#"><i class="fab fa-instagram"></i></a></div>
+                </div>
+                <p class="copyright">Company Name © 2018</p>
+            </div>
+        </footer>
+      </div>
+      <script>
+                const clocks = document.getElementsByClassName("clock");
+                
+                function updateClocks() {
+                    for (let clock of clocks) {
+                    let timezone = clock.dataset.timezone;
+                    let time = new Date().toLocaleTimeString("en-US", {
+                        hour: '2-digit',
+                        minute:'2-digit',
+                        timeZone: timezone
+                    });
+                    clock.textContent = time;
+                    }
+                }
+                
+                // Update every minute:
+                setInterval(updateClocks, 60000);
+                updateClocks();
+        </script>
+                    
+</body>
+</html>
