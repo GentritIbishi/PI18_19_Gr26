f4080ace47cf94f81c7a6b47d779e98f412dc596
EldinVeliji <44035030+EldinVeliji@users.noreply.github.com>
2019-05-31 14:40:27 +0200

    Update oneplus6t.php

--- oneplus6t.php
+++ oneplus6t.php
@@ -304,7 +304,11 @@ require "header.php";
                     </td>
                 </tr>
                 </main> 
-
+</table>
+<br>
+ <?php
+      include 'comment-section.php';
+  ?>
 
 
 </body>
